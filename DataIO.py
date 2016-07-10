16f673f6a8d35d4c81c2c9820451bfbedff9fca3
KuanLiang <kliangliu@gmail.com>
2016-07-10 12:22:04 +0800

    DataIO.py test ok

---
+++ DataIO.py
@@ -0,0 +1,13 @@
+import os
+import pandas as pd
+
+def csvToDf(path):
+    # import os
+    dirlist = os.listdir(path)
+    dfList = []
+    for excelfile in dirlist:
+        if os.path.splitext(excelfile)[1] == '.csv':
+            #print excelfile
+            path = './tear/' + excelfile
+            dfList.append((pd.read_csv(path, sep=';'), excelfile))
+    return dfList
