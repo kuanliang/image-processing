0fcaeaada09a9f7ee641e1080cb06d9575de397a
KuanLiang <kliangliu@gmail.com>
2016-07-14 09:45:47 +0800

    add SVM modeling

--- Model.py
+++ Model.py
@@ -12,32 +12,72 @@ import random
 import pandas as pd
 import numpy as np
 
-def logistic(X, y, random_state=0):
+def logisticModeling(dfTrain, dfTest, random_state=0):
+    '''perform logistic regression
     
+    Notes: 
+    
+    Args:
+        dfTrain: training dataframe split according to SN split
+        dfTest: testing dataframe split according to SN split
+    
+    Return: trained logistic model
+    
+    '''
     pipe = Pipeline(
         [
             ('imputer', Imputer(missing_values='NaN', strategy='median', axis=1)),
             ('scaler', RobustScaler()),
-            ('clf', linear_model.LogisticRegression(penalty='l1'))
+            ('clf', linear_model.LogisticRegression(class_weight='balanced', max_iter=1000))
         ]
     )
     param_grid = {
-        #'clf__penalty': ('l1'),
+        'clf__penalty': ('l1','l2'),
         'clf__tol': (1, 1e-1, 1e-2, 1e-3, 1e-4),
         'clf__C': (10, 5, 1, 0.1, 0.01, 0.001, 0.0001)
     }
     
     
-    X_train, X_test, y_train, y_test = cross_validation.train_test_split(mlDf.iloc[:,0:-1], mlDf['label'], test_size = 0.3, random_state=0)
+    # X_train, X_test, y_train, y_test = cross_validation.train_test_split(mlDf.iloc[:,0:-1], mlDf['label'], test_size = 0.3, random_state=0)
     
     gs_cv = grid_search.GridSearchCV(pipe, param_grid, scoring='f1_weighted')
+    
+    X_train, y_train = dfTrain.iloc[:, 0:-1], dfTrain.iloc[:,-1]
+    X_test, y_test = dfTest.iloc[:, 0:-1], dfTest.iloc[:, -1]
     
     gs_cv.fit(X_train, y_train)
     
     y_predict = gs_cv.predict(X_test)
     
-    return gs_cv
+    return gs_cv, (y_test, y_predict)
+
+
+def SVMModeling(dfTrain, dfTest, random_state=0):
     
+    pipe = Pipeline([
+        #('union', InitialProcessor(colInfo=colInfo)),
+        ('impute', Imputer(missing_values = 'NaN', strategy = 'median', axis = 1)),
+        ('scaler', RobustScaler()),
+        #('clf', linear_model.SGDClassifier(loss = 'log', penalty='l1', class_weight='balanced', n_iter=1))
+        # ('pca', pca)
+        ('clf', SVC(kernel='rbf', class_weight='balanced'))
+    ])
+
+    param_grid = {
+        'clf__C': (1, 1e2, 1e3, 1e4, 1e5),
+        'clf__gamma': (1, 1e-1, 1e-2, 1e-3, 1e-4),
+        'clf__tol': (1e-2, 1e-3, 1e-4, 1e-5)
+    }
+    gs_cv = grid_search.GridSearchCV(pipe, param_grid, scoring='f1_weighted')
+    
+    X_train, y_train = dfTrain.iloc[:, 0:-1], dfTrain.iloc[:,-1]
+    X_test, y_test = dfTest.iloc[:, 0:-1], dfTest.iloc[:, -1]
+    
+    gs_cv.fit(X_train, y_train)
+    
+    y_predict = gs_cv.predict(X_test)
+    
+    return gs_cv, (y_test, y_predict)
 
 def train_test_split(df, trainRatio=0.6, testRatio=0.4):
     '''split training and testing data according to 3 cases (drop, duplicate, tear)
@@ -64,7 +104,7 @@ def train_test_split(df, trainRatio=0.6, testRatio=0.4):
     tearTrainDf = pd.concat([df[df.index.str.contains(SN)] for SN in labelSNDictTrain['tear']])
     
     trainDf = pd.concat([dropTrainDf, duplicateTrainDf, tearTrainDf])
-    testDf = df.loc[list(set(df.index) - set(df.index))]
+    testDf = df.loc[list(set(df.index) - set(trainDf.index))]
     
     
     return trainDf, testDf
