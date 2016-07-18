f5f9d9625d7d30c09d0cfea642bb34bf36133b44
KuanLiang <kliangliu@gmail.com>
2016-07-18 10:50:04 +0800

    add predict function and plot

--- Predict.py
+++ Predict.py
@@ -1,16 +1,20 @@
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 from sklearn.metrics import accuracy_score
-
+from Plot import plot_probability
 
 def predict_query(mlDf, model, column):
     '''predict query according to specified model
     
-    Notes:
+    Notes: 
     
     Argus:
+        mlDf:
+        model:
+        column:
     
     Return:
+        predict result
     
     '''
     predict = model.predict(mlDf[column].iloc[:,0:-1])
@@ -19,4 +23,6 @@ def predict_query(mlDf, model, column):
     accuracy = accuracy_score(mlDf[column].iloc[:,-1], predict)
     
     print accuracy
-    plot_tear_probability(mlDf, predict_prob)
+    plot_probability(mlDf, predict_prob)
+    
+    return predict
